2ffe5d3ecc6ab3b48e262be948a8245bc57d46c8
Isabelle <isabelle.bernardes@hotmail.com>
2023-05-20 23:46:25 -0300

    style: add style settings to tailwind.config.js file

--- portfolio/src/components/Header/index.tsx
+++ portfolio/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import logo from "../../public/img/logo-isabellePortfolio.png";
 import darkModeToggle from "../../public/img/Toggle DarkMode--Light.svg";
 import languageToggle from "../../public/img/Toggle Language --Light.svg";
 import Image from "next/image";
-import { MenuItem } from "./fragments/MenuItem";
+import { MenuItem } from "../fragments/MenuItem";
 import Link from "next/link";
 
 export const Header = () => {
