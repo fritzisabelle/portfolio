b77f4d62c3fa2a88db63063a0518b73bcd806da1
Isabelle <isabelle.bernardes@hotmail.com>
2023-05-21 13:10:30 -0300

    fix: fix code structure on intern pages

---
+++ portfolio/src/app/(intern)/contact/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function ContactPage() {
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <h1>Contact Page</h1>
+    </>
+  );
+}
---
+++ portfolio/src/app/(intern)/work/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function AboutMePage() {
+  return(
+    <Link href='/'>Home</Link>
+    <h1 className="font-dazzle">About me Page</h1>
+  )
+}
